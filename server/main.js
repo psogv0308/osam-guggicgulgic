52499eccfe768b4e01f48a00721141a9aba49e30
root <psogv0308@gmail.com>
2019-10-24 09:06:14 +0000

    add authentification module

--- server/main.js
+++ server/main.js
@@ -3,7 +3,6 @@ async function main(audio_file,callback) {
   const speech = require('@google-cloud/speech');
   const fs = require('fs');
   const Chatbot = require('./Chatbot');
-
   // Creates a client
   const client = new speech.SpeechClient();
 
@@ -25,7 +24,7 @@ async function main(audio_file,callback) {
     languageCode: 'ko-KR',
 	speechContexts: [
 		{
-			phrases: ["이건철", "기반체계운용대", "피엑스","80대대 지휘통제실","80대대 지통실","의무대","인트라넷계정"]
+			phrases: ["배차신청", "탄약","의무대","지통실","군의관","훈련","암구호","컴퓨터","프린터","국군대전병원","국군수도병원","직속상관","출장신청","어 난데","탄약 신청","개인신상","PX","사령관","자비스","전역일"]
 		}
 	]
   };
@@ -44,16 +43,18 @@ async function main(audio_file,callback) {
   //console.log(`${tr}`);
   if(tr == null || tr == '') {
 	console.log("sound undetected");
-    callback("sound undetected");
+    callback("Zzz","0. sound undetected");
   }
   else{
     Chatbot.chatbot(transcription,function(msg){
 	  console.log(msg);
-	  callback(msg);
+		//들리지 않았을때 예외처리
+	  callback(transcription,msg);
     });
   }
 }
 
+var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
 var server = app.listen(3000, function(){
@@ -62,11 +63,42 @@ var server = app.listen(3000, function(){
 
 var multer = require('multer');
 var upload = multer({ dest: 'uploads/' })
+var talk_data = ""
+var swi = 1;
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
 app.get('/', function(req, res){
     res.send('Hello World');
 });
+app.get('/app', (req, res) => {
+	res.send(talk_data);
+	console.log("ok");
+});
+app.get('/switch', (req, res) => {
+	res.send("switch");
+	swi=1-swi;
+	console.log(swi);
+});
 app.post('/upload', upload.single('file'), function(req, res){
-  main(req.file,function(msg){
-	  res.send(msg);
+  main(req.file,function(tr, msg){
+	  console.log(msg);
+	  var str=msg.split(". ");
+	  if(swi==0 && req.body.user=="1"){
+		  res.send("Jarvis is turned off");
+	  	  talk_data+=tr+"\nJarvis is turned off\n";
+	  }
+	  else if(req.body.user>=str[0]){
+        res.send(str[1]);  
+	    if(req.body.user=="1"){
+	  	  talk_data+=tr+"\n"+str[1]+"\n";
+	    }
+	  }
+	  else{
+		if(req.body.user=="1") 
+	  	  talk_data+=tr+"\npermission denied\n";
+		res.send("permission denied")
+	  }
   }).catch(console.error);
 });
